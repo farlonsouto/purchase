d6e84050b20312c11119a2a0c9de240485b7982f
Farlon Souto <farlonsouto@gmail.com>
2026-04-28 00:15:43 +0200

    Gives it unique path mapping

--- order-service/src/main/java/com/example/order/controller/OrderViewController.java
+++ order-service/src/main/java/com/example/order/controller/OrderViewController.java
@@ -1,24 +1,23 @@
 package com.example.order.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class OrderViewController {
 
   private final List<String> orders = new ArrayList<>();
 
-  @GetMapping("/orders")
+  @GetMapping("/view/orders")
   public String getOrders(Model model) {
     model.addAttribute("orders", orders);
     return "orders";
   }
 
-  @PostMapping("/orders")
+  @PostMapping("/view/orders")
   public String createOrder(@RequestParam String product) {
     orders.add(product);
     return "redirect:/orders";
